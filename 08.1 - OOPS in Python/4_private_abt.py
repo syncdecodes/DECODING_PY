51d926d4d394e62c71343ff08d120d8391425ed7
syncdecodes <syncdecodes@gmail.com>
2025-12-30 12:07:08 +0530

    Update 4_private_abt.py

--- 08.1 - OOPS in Python/4_private_abt.py
+++ 08.1 - OOPS in Python/4_private_abt.py
@@ -1,4 +1,4 @@
-# To make a attribute or method private use __
+# To make a priveate attribute or method private use __
 class Account:
     def __init__(self, acc_no, acc_pass):
         self.acc_no = acc_no
@@ -29,5 +29,6 @@ print(p1.welcome())
 
 # PRIVATE ATTRIBUTES AND METHODS ARE MEANT TO BE USED ONLY WITHIN THE CLASS AND ARE NOT ACCESSIBLE FROM OUTSIDE THE CLASS.
 # AGAR HUMNE ATTRIBUTES YA METHODS KO DOUBLE UNDERSCORE ( __ ) SE PRIVATE BANA DIA TOH USE CLASS KE SIRF JO INTERNAL FUNCTION HAI WAHI ACCESS KAR PAENGE BAHAR KOI USE ACCESS NAHI KAR PAEGA.
+
 
 
